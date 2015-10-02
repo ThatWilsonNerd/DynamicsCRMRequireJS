1a9161e5a02386b70f8a72dec58e861464f9a213
Will Wilson <willjpw@gmail.com>
2015-10-02 14:41:39 -0400

    Updated CustomerEmbed.js

--- new_CustomerEmbed.js
+++ new_CustomerEmbed.js
@@ -1,8 +1,14 @@
 //  use RequireJS to ensure Knockout is loaded prior to running our stuff
-requirejs(['new_knockout'], function(ko) {
-  //  create our view model
-  var Model 
+requirejs(['new_knockout','new_XrmServiceToolkit'], function(ko) {
+ 	//  create our view model
+	function Model() {
+		var self = this;
+		self.fullname = ko.observable(null);
+		
+		// do some stuff to get the data from the customer (assuming a contact)
+		self.fullname('Billy Bob');
+	}
   
-  //  apply it
-  ko.applyBindings(new Model());
+ 	//  apply it
+ 	ko.applyBindings(new Model());
 });
